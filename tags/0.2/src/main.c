45429bfed0333f63f2880578618a31cf11dfc3d6
rush <rush@3971b2cb-9d2c-4bc5-b62c-c6a8cd69d2e5>
2009-03-20 14:25:30 +0000

    0.2

---
+++ tags/0.2/src/main.c
@@ -0,0 +1,250 @@
+/***************************************************************************
+ *            main.c
+ *
+ *  Wed Jan 16 11:25:01 2008
+ *  Copyright  2008  winkey
+ *  <rush@winkey.org>
+ ****************************************************************************/
+
+/*
+ * main.c
+ * Copyright (C) winkey 2008 <rush@winkey.org>
+ * 
+ * main.c is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * main.c is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gdal_alg.h>
+
+#include "grib.h"
+#include "gdalcode.h"
+#include "ogrcode.h"
+
+
+OGRLayerH contour (
+	OGRSpatialReferenceH hSRS,
+	OGRDataSourceH hDS,
+	double interval,
+	GDALRasterBandH hBand,
+	int id,
+	int elev)
+{
+
+	/***** create the layer *****/
+	
+	OGRLayerH hLayer = create_layer(hDS, "contour", hSRS, wkbLineString);
+	
+	/***** id field *****/
+	
+	create_field("ID", OFTInteger, 8, 0, hLayer);
+	
+	/***** value field *****/
+	
+	create_field("elev", OFTReal, 12, 3, hLayer);
+	
+	/***** contour *****/
+	
+	GDALContourGenerate(hBand, interval, 0.0, 0, NULL,
+											FALSE, 0.0, hLayer, id, elev, NULL, NULL);
+	
+	return hLayer;
+}
+
+void transform(
+	OGRSpatialReferenceH hMemSRS,
+	OGRSpatialReferenceH hKmlSRS,
+	OGRLayerH hMemLayer,
+	OGRLayerH hKmlLayer)
+{
+	/***** Setup coordinate transformation *****/
+	
+	OGRCoordinateTransformationH *hTransform = 
+		create_coord_transform(hMemSRS, hKmlSRS);
+	
+	/***** reset the source layer *****/
+	
+	OGR_L_ResetReading(hMemLayer);
+	
+	/***** loop while theres features *****/
+	
+	OGRFeatureH hSrcFeat;
+	
+	while((hSrcFeat = OGR_L_GetNextFeature(hMemLayer))) {
+		
+		CPLErrorReset();
+		
+		/***** make a new feature *****/
+		
+		OGRFeatureH hDstFeat = OGR_F_Create(OGR_L_GetLayerDefn(hKmlLayer));
+		
+		/***** translate feature *****/
+		
+		translate_feature(hDstFeat, hSrcFeat);
+		
+		
+		/***** transform the feature if it translated *****/
+		
+		transform_feature(hDstFeat, hTransform);
+		
+		CPLErrorReset();
+		
+		/***** create the new feature in the layer *****/
+		
+		add_feature(hKmlLayer, hDstFeat);
+		
+		/***** cleanup *****/
+		
+		OGR_F_Destroy(hSrcFeat);
+		OGR_F_Destroy(hDstFeat);
+	}
+	
+	/***** cleanup *****/
+	
+	OCTDestroyCoordinateTransformation(hTransform);
+	
+	return;
+}
+
+
+int main(int argc, char **argv)
+{
+  FILE *fp = NULL;
+  float *raster = NULL;
+	
+  gds_t gds = {};
+	
+  GDALDatasetH hDS;
+	
+	/***** init *****/
+	
+	GDALAllRegister();
+	OGRRegisterAll();
+  
+  /***** check args *****/
+  
+  if (argc < 5 || argc > 5) {
+	fprintf(stderr, "USAGE: %s <grib file> <grib msg> <interval> <kml file>\n",
+			argv[0]);
+	exit(EXIT_FAILURE);
+  }
+  
+  /***** open the grib file *****/
+  
+  fp = grib_open(argv[1], atoi(argv[2]), 'm');
+  
+  /***** read the grib raster into memory *****/
+  
+  raster = grib_read(fp, &gds);
+  
+	/***** close the grib file *****/
+	
+	pclose(fp);
+	
+  /***** open the raster in memory as a gdal data set *****/
+  
+  hDS = raster_open_mem(raster, gds.Nx, gds.Ny);
+  
+  /***** set the projection *****/
+  
+  OGRSpatialReferenceH hSrcSRS = set_projection(hDS, &gds);
+  
+	/***** get the raster band *****/
+	
+	GDALRasterBandH hBand = get_band(hDS, 1);
+	
+	/***** set the spatial reference for kml *****/
+		
+	OGRSpatialReferenceH hKmlSRS = OSRNewSpatialReference(NULL);
+	OSRSetWellKnownGeogCS(hKmlSRS, "WGS84");
+	
+	/***** get the ogr driver *****/
+	
+	OGRSFDriverH hKmlDriver = get_driver("KML");
+	
+	/***** create the datasource *****/
+	
+	OGRDataSourceH hKmlDS = create_datasource(hKmlDriver, argv[4]);
+	
+	/***** check to see if we can go strait to kml *****/
+	
+	if (gds.proj == GDS_LATLON || gds.proj == GDS_GAUSSIAN_LATLON) {
+		
+		contour (hKmlSRS, hKmlDS, atof(argv[3]), hBand, 2, 3);
+		
+	}
+	
+	/***** ok not a simple lat lon raster *****/
+	
+	else {
+		
+		/***** set the spatial reference for mem *****/
+		
+		OGRSpatialReferenceH hMemSRS = hSrcSRS;
+		char *wkt;
+		
+		OSRExportToWkt(hMemSRS, &wkt);
+		//fprintf(stderr, "SrcWKT =\n%s\n\n", wkt);
+		
+		/***** get the ogr driver *****/
+	
+		OGRSFDriverH hMemDriver = get_driver("Memory");
+	
+		/***** create the datasource *****/
+	
+		OGRDataSourceH hMemDS = create_datasource(hMemDriver, argv[4]);
+		
+		/***** contour *****/
+		
+		OGRLayerH hMemLayer = contour (hMemSRS, hMemDS, atof(argv[3]), hBand, 0, 1);
+		
+		/***** create the layer to trasnslate too *****/
+		
+		OGRLayerH hKmlLayer = create_layer(hKmlDS, "contour", hKmlSRS, wkbLineString);
+		
+			/***** id field *****/
+	
+		create_field("ID", OFTInteger, 8, 0, hKmlLayer);
+		
+		/***** value field *****/
+		
+		create_field("elev", OFTReal, 12, 3, hKmlLayer);
+
+		/***** transform from mem to kml *****/
+		
+		transform(hMemSRS, hKmlSRS, hMemLayer, hKmlLayer);
+		
+		/***** cleanup *****/
+		
+		OGR_DS_Destroy(hMemDS);
+	}
+
+	/***** cleanup *****/
+		
+	OSRDestroySpatialReference(hSrcSRS);
+	OSRDestroySpatialReference(hKmlSRS);
+	
+	OGR_DS_Destroy(hKmlDS);
+	
+	OGRCleanupAll();
+	/***** cleaup raster stuff *****/
+	
+	GDALClose(hDS);
+	
+	free(raster);
+	GDALDestroyDriverManager();
+	
+  return EXIT_SUCCESS;
+}
---
+++ tags/0.2/src/ogrcode.h
@@ -0,0 +1,175 @@
+/***************************************************************************
+ *            ogrcode.h
+ *
+ *  Wed Jan 16 11:25:01 2008
+ *  Copyright  2008  winkey
+ *  <rush@winkey.org>
+ ****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+ 
+#ifndef _OGRCODE_H
+#define _OGRCODE_H
+
+#include <gdal.h>
+#include <cpl_conv.h>
+#include <ogr_api.h>
+#include <ogr_srs_api.h>
+
+
+/*******************************************************************************
+	function to get the spacial referance from a raster
+	
+	args:
+							band		gdal raster band
+	
+	returns:
+							ogr spatial reference
+*******************************************************************************/
+
+OGRSpatialReferenceH get_srs(
+	GDALDatasetH hband);
+
+/*******************************************************************************
+	function to fetch a ogr driver
+
+	args:
+						driver		the name of the ogr driver
+	
+	returns:
+						OGRSFDriverH
+*******************************************************************************/
+
+OGRSFDriverH get_driver(
+	char *driver);
+
+/*******************************************************************************
+	create the ogr output data source
+	
+	args:
+						hDriver			the ogr driver
+						dst_file		the destination file name
+	
+	returns:
+						the ogr data source
+*******************************************************************************/
+
+OGRDataSourceH create_datasource(
+	OGRSFDriverH hDriver,
+	char *dst_file);
+
+/*******************************************************************************
+	function to create the ogr layer
+	
+	args:
+							hDS			the ogr datasource
+							name		the name of the layer
+							hSRS		the spatial reference
+							type		the geometry type
+	
+	returns:
+							a new ogr layer
+*******************************************************************************/
+
+OGRLayerH create_layer(
+	OGRDataSourceH hDS,
+	const char *name,
+	OGRSpatialReferenceH hSRS,
+	OGRwkbGeometryType type);
+
+
+/*******************************************************************************
+	function to create a ogr field
+	
+	args:
+							name				the name of the field to create
+							eType				the field data type
+							width				the field data width
+							precision		the field data precision
+							hLayer			the layer to create the field in
+	
+	returns:
+							nothing
+*******************************************************************************/
+
+void create_field(
+	const char *name,
+	OGRFieldType eType,
+	int width,
+	int precision,
+	OGRLayerH hLayer);
+
+/*******************************************************************************
+	function to set up coord transformation
+	
+	args:
+						hSrcSRS the source spatial reference
+						hDstSRS the destination spatial reference
+
+	returns:
+						a new coordanate transformation
+*******************************************************************************/
+
+OGRCoordinateTransformationH *create_coord_transform(
+	OGRSpatialReferenceH hSrcSRS,
+	OGRSpatialReferenceH hDstSRS);
+
+/*******************************************************************************
+	function to translate a feature from one ogr format to another
+	
+	args:
+							hDstFeat		the destination feature
+							hSrcFeat		the source feature
+
+	returns:
+							nothing
+*******************************************************************************/
+
+void translate_feature(
+	OGRFeatureH hDstFeat,
+	OGRFeatureH hSrcFeat);
+
+/*******************************************************************************
+	function to transform the geometry of a feature
+	
+	args:
+							hDstFeat		the destination feature
+							hTransform	the transformer
+	returns:
+							nothing
+*******************************************************************************/
+
+void transform_feature(
+	OGRFeatureH hDstFeat,
+	OGRCoordinateTransformationH *hTransform);
+
+/*******************************************************************************
+	function to add a new feature to a layer
+	
+	args:
+							hDstLayer		the layer to add a new feature too
+							hDstFeat		the feature to add
+	returns:
+							nothing
+*******************************************************************************/
+
+void add_feature(
+	OGRLayerH hDstLayer,
+	OGRFeatureH hDstFeat);
+
+
+
+#endif /* _OGRCODE_H */
