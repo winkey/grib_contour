c4ce02be8d9098c23f90844fb792ebaef5d20b32
rush <rush@3971b2cb-9d2c-4bc5-b62c-c6a8cd69d2e5>
2009-03-20 19:30:06 +0000

    0.27

---
+++ tags/0.27/scripts/forecaster.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+
+source /usr/local/bin/generic2.sh
+
+wwwdisk="/home/rush/public_html/weather/kml/forecaster"
+www="/home/rush/public_html/weather/kml/forecaster"
+
+#NGRID ^[LM]DG[A-Z]98 KNHC .* !grib2/ncep/FORECASTER/#000/([0-9]{8})([0-9][0-9])00F[0-9]{3}/SPED([0-9]{2,3})00(17|25|32)P.*
+#	FILE	data/ngrid/forecaster/\1\2.\2.\3.\4.grib2
+#NGRID ^[LM]DG[A-Z]98 KNHC .* !grib2/ncep/FORECASTER/#000/([0-9]{8})([0-9][0-9])00F[0-9]{3}/SPED([0-9]{2,3})00(17|25|32)P.*
+#	EXEC	/usr/local/bin/forecaster.sh data/ngrid/forecaster/\1\2.\2.\3.\4.grib2 \2 \3 \4
+
+path="$1"
+run="$2"
+hour="$3"
+prod="$4"
+
+tmp="/tmp"
+
+if [[ "$hour" == "" ]]
+then
+	hour="00"
+fi
+
+if [[ ! -d "${wwwdisk}/${run}" ]]
+then
+	mkdir -p "${wwwdisk}/${run}"
+fi
+
+frames=${wwwdisk}/${run}/prob${prod}.kml
+
+if [[ $(($(stat -c %Y ${frames})+21600)) -lt $(date "+%s") ]]
+then
+	mkrootkml "prob${prod}"
+fi
+	
+zip="${wwwdisk}/${run}/prob${prod}${hour}.kmz"
+kml="prob${prod}${hour}.kml"
+
+if [[ -f "$zip" ]]
+then
+	rm "$zip"
+fi
+	
+nice -n 10 grib_contour -g "${path}" -m 1 -i 5 -s prob -k $kml -z "$zip"
+appendkml "prob${prod}" $hour 06
+	
+rm "$path"
---
+++ tags/0.27/share/ruc2.config.sh
@@ -0,0 +1,120 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+
+name=ruc2
+fullname="20 km Resolution CONUS RUC2 Forecast Model"
+
+description='Data from <a href=\"http://www.ncep.noaa.gov/\">
+National Centers for Environmental Prediction</a> 
+generated twenty four times daily is contoured and converted to kml
+for forecast hours 0-9 in 1 hour increments.'
+
+levels="300 300 mb
+500 500 mb
+700 700 mb
+850 850 mb
+925 925 mb"
+
+products300="wind Wind
+t Temperature
+rh Relative Humidity
+hgt Heights"
+
+products500="wind Wind
+t Temperature
+rh Relative Humidity
+hgt Heights"
+
+products700="wind Wind
+t Temperature
+rh Relative Humidity
+hgt Heights"
+
+products850="wind Wind
+t Temperature
+rh Relative Humidity
+hgt Heights"
+
+products925="wind Wind
+t Temperature
+rh Relative Humidity
+hgt Heights"
+
+
+levels2="0 0 - none"
+
+products2="pwat Perceptible Water
+pmsl Mean Sea Level Pressure
+cape Convective Available Potential Energy"
+
+times="00 00Z
+01 01Z
+02 02Z
+03 03Z
+04 04Z
+05 05Z
+06 06Z
+07 07Z
+08 08Z
+09 09Z
+10 10Z
+11 11Z
+12 12Z
+13 13Z
+14 14Z
+15 15Z
+16 16Z
+17 17Z
+18 18Z
+19 19Z
+20 20Z
+21 21Z
+22 22Z
+23 23Z"
+
+screenshots=$(
+  echo "$levels" | while read lev_n lev_fn
+  do
+		ref="products$lev_n"
+    echo "${!ref}" | while read prod_n prod_fn
+    do
+      echo ${name}_${lev_n}${prod_n}.jpg
+    done
+  done
+  echo "$levels2" | while read lev_n lev_fn
+  do
+    echo "$products2" | while read prod_n prod_fn
+    do
+      echo ${name}_${prod_n}.jpg
+    done
+  done
+)
+
+thumbs=$(
+  echo "$levels" | while read lev_n lev_fn
+  do
+		ref="products$lev_n"
+    echo "${!ref}" | while read prod_n prod_fn
+    do
+      echo ${name}_${lev_n}${prod_n}.png
+    done
+  done
+  echo "$levels2" | while read lev_n lev_fn
+  do
+    echo "$products2" | while read prod_n prod_fn
+    do
+      echo ${name}_${prod_n}.png
+    done
+  done
+)
