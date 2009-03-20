8b6bf327cd6291a1a83f40ac54032c8056243917
rush <rush@3971b2cb-9d2c-4bc5-b62c-c6a8cd69d2e5>
2009-03-20 14:33:32 +0000

    0.6

---
+++ tags/0.6/src/grib.h
@@ -0,0 +1,115 @@
+/***************************************************************************
+ *            grib.h
+ *
+ *  Wed Jan 16 11:25:01 2008
+ *  Copyright  2008  winkey
+ *  <rush@winkey.org>
+ ****************************************************************************/
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+ 
+#ifndef _GRIB_H
+#define _GRIB_H
+
+/*******************************************************************************
+	struct for the gds info in the grib file
+*******************************************************************************/
+
+typedef struct {
+	int Npoints;
+	int proj;
+	char shape[21];
+	float radius;
+	char radius_units[21];
+	int Nx;
+	int Ny;
+	float Lat1;
+	float Lon1;
+	float Lat2;
+	float Lon2;
+	float Dx;
+	char Dx_units[21];
+	float Dy;
+	char Dy_units[21];
+	float meshlat;
+	float orientlon;
+	float tan_Lat1;
+	float tan_Lat2;
+	float S_lat;
+	float S_lon;
+} gds_t;
+
+/*******************************************************************************
+	enum for grib projection types
+*******************************************************************************/
+
+enum {
+	GDS_LATLON = 0,
+	GDS_MERCATOR = 10,
+	GDS_POLAR = 20,
+	GDS_LAMBERT = 30,
+	GDS_GAUSSIAN_LATLON = 40,
+	GDS_ORTHOGRAPHIC = 90,
+	GDS_ROTATED_LATLON = 100,
+	GDS_EQUATOR_EQUIDIST = 110,
+	GDS_AZIMUTH_RANGE = 120
+};
+
+/*******************************************************************************
+	function to open the grib file
+
+	args:
+							gribfile		the filename of the gribfile to open
+							msg					the grib message to decode
+							unit				unit type to decode to 'e' or 'm'
+	
+	returns:
+							file pointer to the stream to read
+*******************************************************************************/
+
+FILE *grib_open(
+	char *gribfile,
+	float msg,
+	char unit);
+
+
+/*******************************************************************************
+	function to read a grib file
+	
+	args:
+							fp		file pointer to the stream to read
+							gds		pointer to the gds struct to store the gds info in
+
+	returns:
+							pointer to the newly allocated raster data
+*******************************************************************************/
+
+float *grib_read(
+	FILE *fp,
+	gds_t *gds);
+
+float *do_grib(
+	options *o,
+	gds_t *gds);
+
+float *do_wind_grib(
+	options *o,
+	gds_t *gds);
+
+#endif /* _GRIB_H */
+
+ 
---
+++ tags/0.6/src/main.c
@@ -0,0 +1,112 @@
+/***************************************************************************
+ *            main.c
+ *
+ *  Wed Jan 16 11:25:01 2008
+ *  Copyright  2008  winkey
+ *  <rush@winkey.org>
+ ****************************************************************************/
+
+/*
+ * main.c
+ * Copyright (C) winkey 2008 <rush@winkey.org>
+ * 
+ * main.c is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * main.c is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include "options.h"
+#include "grib.h"
+
+#include "buffer.h"
+#include "color.h"
+#include "ogrcode.h"
+#include "gdalcode.h"
+#include "contour.h"
+#include "zipbuffer.h"
+#include "kml.h"
+
+
+
+int main(int argc, char **argv)
+{
+  options o = {};
+	
+  float *raster = NULL;
+	gds_t gds = {};
+	GDALDatasetH hDS;
+	GDALRasterBandH hBand;
+	OGRSpatialReferenceH hSRS;
+	color_scale *cscales;
+	OGRDataSourceH hogrDS;
+	OGRLayerH hLayer;
+	
+	/***** init *****/
+	
+	GDALAllRegister();
+	OGRRegisterAll();
+  
+  /***** check args *****/
+  
+	get_options(argc, argv, &o);
+  
+	/***** get the raster *****/
+	
+	if (o.wind)
+		raster = do_wind_grib(&o, &gds);
+	else
+		raster = do_grib(&o, &gds);
+	
+	/***** get the color scale *****/
+	
+	cscales = color_getscale(o.scalename);
+	
+	
+	hDS = do_gdal(raster, &gds, &hSRS, &hBand);
+  
+	if (o.tiffile) {
+		
+		/***** make an image *****/
+		
+	}
+	
+	if (o.kmlfile) {
+		
+		/***** make a linestring *****/
+		
+		contour (hDS, hSRS, &hogrDS, &hLayer, o.interval, 0, 1);
+		
+		
+		contour2kml(&gds, &o, cscales, hSRS, hBand, hLayer);
+		
+		OSRDestroySpatialReference(hSRS);
+		
+		OGR_DS_Destroy(hogrDS);
+	
+		OGRCleanupAll();
+		}
+	
+	free(cscales);
+	
+	/***** cleaup raster stuff *****/
+	
+	GDALClose(hDS);
+	
+	free(raster);
+	GDALDestroyDriverManager();
+	
+  return EXIT_SUCCESS;
+}
