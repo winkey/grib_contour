f416cbab6b7d32956885b2a9c176c2815d7b4d1c
rush <rush@3971b2cb-9d2c-4bc5-b62c-c6a8cd69d2e5>
2009-03-20 14:35:14 +0000

    0.10

---
+++ tags/0.10/src/color.h
@@ -0,0 +1,56 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+ 
+#ifndef _COLOR_H
+#define _COLOR_H
+
+typedef struct {
+  float value;
+  char color[7];
+} color_scale;
+
+/*******************************************************************************
+	function to get the scale
+
+	args:
+							name				the name of the scale
+
+	returns:
+							the scale
+	
+	NOTE: you must free() the result when your done with it
+*******************************************************************************/
+
+color_scale *color_getscale(
+	char *name);
+
+/*******************************************************************************
+	function to get a color from a scale
+
+	args:
+							scales			the scale to check
+							value				the value to check for in the scale
+
+	returns:
+							the color for the value
+*******************************************************************************/
+ 
+char *color_checkscale(
+	color_scale *scales,
+	float value);
+
+#endif /* _COLOR_H */
+
